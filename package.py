3df53069b7cc49f3a4e711bc9c30aeba356d1ab6
Heorhi Samushyia <george@brudanstudios.com>
2023-03-13 20:38:33 +0000

    remove variants

--- package.py
+++ package.py
@@ -9,10 +9,5 @@ private_build_requires = ["rezutil"]
 requires = ["bd.hooks", "PyYAML-5.4.1+", "schema-0.7.1+", "six"]
 
 
-variants = [["python-3"], ["python-2", "futures"]]
-
-hashed_variants = True
-
-
 def commands():
     env.PYTHONPATH.append("{root}/python")
