ea4ab0c2a07aa47f26c3f6f0d50d81c64c37b3d0
Heorhi Samushyia <george@brudanstudios.com>
2023-09-26 09:58:51 +0100

    add external cachetools requirement

--- package.py
+++ package.py
@@ -1,12 +1,12 @@
 name = "bd.storage"
 
-version = "0.2.11"
+version = "0.2.12"
 
 build_command = "python -m rezutil build {root} --ignore .env"
 private_build_requires = ["rezutil"]
 
 
-requires = ["bd.hooks", "PyYAML-5.4.1+", "schema-0.7.1+", "six"]
+requires = ["bd.hooks", "PyYAML-5.4.1+", "schema-0.7.1+", "six", "cachetools-3.1.1"]
 
 
 def commands():
